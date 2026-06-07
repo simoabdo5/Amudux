fdc2da682e10e39490d52d5012a2f0a28b978719
daoudi <daoudimohamed838@gmail.com>
2026-06-07 20:00:41 +0000

    modification dial branch daoudi destination et delet favorit

--- backend/app/Models/Restaurant.php
+++ backend/app/Models/Restaurant.php
@@ -21,11 +21,4 @@ class Restaurant extends Model
     {
         return $this->belongsTo(City::class);
     }
-<<<<<<< HEAD
-=======
-    public function favorites()
-{
-    return $this->morphMany(Favorite::class, 'favoritable');
-}
->>>>>>> 27cef02d8a3c291f193d74224d98c69aa22d46a8
 }
